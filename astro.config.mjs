fa46ac68b3f38c799f9accefbd2412aad5496ac1
Bryce Wray <bwray@wraytx.com>
2022-04-04 12:16:08 -0500

    Back to using `public` for static assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.brycewray.com/',
-  // publicDir: './publicDir',
   markdown: {
     remarkPlugins: [
       ["remark-smartypants", {
