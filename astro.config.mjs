86e39449535dbe6326691cb57a6c194d2e111007
Bryce Wray <bwray@wraytx.com>
2022-04-16 13:08:58 -0500

    Sitemap capability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,33 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.brycewray.com/',
+  site: 'https://www.brycewray.com/', // commenting this out prevents XML sitemap from happening during dev
   server: {
     port: 3000,
-    host: "192.168.254.10",
+    host: "192.168.254.10"
   },
   trailingSlash: "always",
   markdown: {
-    remarkPlugins: [
-      ["remark-smartypants", {
-        dashes: "oldschool",
-        ellipses: false,
-      }],
-      "remark-footnotes",
-    ],
-    rehypePlugins: [
-      "rehype-external-links",
-      "rehype-slug",
-      ["rehype-autolink-headings", {
-        behavior: "wrap",
-        // test: [ 'article h1', 'article h2', 'article h3', 'article h4', 'article h5', 'article h6']
-      }]
-    ],
+    remarkPlugins: [["remark-smartypants", {
+      dashes: "oldschool",
+      ellipses: false
+    }], "remark-footnotes"],
+    rehypePlugins: ["rehype-external-links", "rehype-slug", ["rehype-autolink-headings", {
+      behavior: "wrap" // test: [ 'article h1', 'article h2', 'article h3', 'article h4', 'article h5', 'article h6']
+
+    }]],
     syntaxHighlight: 'prism',
     shikiConfig: {
-      wrap: true,
-    },
+      wrap: true
+    }
   },
   vite: {
     ssr: {
-      external: ['twitter-api-client'],
-    },
+      external: ['twitter-api-client']
+    }
   },
-})
+  integrations: [sitemap()]
+});
