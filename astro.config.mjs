59410817ea7d188f96a2a7216dc25b9969825e7b
Bryce Wray <bwray@wraytx.com>
2022-04-24 14:56:51 -0500

    Adding astro-critters (for Critical CSS)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,36 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from "@astrojs/sitemap"
+import astro from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.brycewray.com/', // commenting this out prevents XML sitemap from happening during dev
+  site: 'https://www.brycewray.com/',
+  // commenting this out prevents XML sitemap from happening during dev
   server: {
     port: 3000,
     host: "192.168.254.10"
   },
   trailingSlash: "always",
   markdown: {
-    remarkPlugins: [
-      ["remark-smartypants", {
-        dashes: "oldschool",
-        ellipses: false
-      }],
-      "remark-footnotes",
-      "remark-gfm"
-    ],
+    remarkPlugins: [["remark-smartypants", {
+      dashes: "oldschool",
+      ellipses: false
+    }], "remark-footnotes", "remark-gfm"],
     rehypePlugins: ["rehype-external-links", "rehype-slug", ["rehype-autolink-headings", {
       behavior: "wrap" // test: [ 'article h1', 'article h2', 'article h3', 'article h4', 'article h5', 'article h6']
 
     }]],
     // syntaxHighlight: 'prism',
     shikiConfig: {
-      theme: 'github-dark',
-      // wrap: true,
+      theme: 'github-dark' // wrap: true,
+
     }
   },
   vite: {
     plugins: [],
-    ssr: {},
-    // logLevel: 'silent' // while I'm working on JSON files!
+    ssr: {} // logLevel: 'silent' // while I'm working on JSON files!
+
   },
-  integrations: [sitemap()]
+  integrations: [sitemap(), astro()]
 });
