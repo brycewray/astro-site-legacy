34e0c97eb53d724cee80ba96f6cb7fa9f4cf5593
Bryce Wray <bwray@wraytx.com>
2022-04-25 09:56:00 -0500

    Astro `generator` and needed JS... ... h/t https://github.com/matthewp/corset-site/

---
+++ src/info.js
@@ -0,0 +1,8 @@
+// borrowing from https://github.com/matthewp/corset-site/blob/main/src/info.js
+
+import { createRequire } from 'module'
+const _require = createRequire(import.meta.url)
+
+// Generator
+const astroPkg = _require('astro/package.json')
+export const generator = `Astro ${astroPkg.version}`
